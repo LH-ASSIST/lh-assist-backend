466fa5e1f6b4f603c424b31417726ef7d7396cea
SinnoLn <ok373737@naver.com>
2026-01-19 15:23:06 +0900

    feat: 도메인 엔티티별 JpaRepository 추가

---
+++ src/main/java/com/lh/assist/domain/document/DocumentRepository.java
@@ -0,0 +1,6 @@
+package com.lh.assist.domain.document;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+}
---
+++ src/main/java/com/lh/assist/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.lh.assist.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
