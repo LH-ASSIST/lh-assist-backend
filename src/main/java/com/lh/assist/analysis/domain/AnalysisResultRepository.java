84b717250fc113b517f89f392cf493ea0136612d
SinnoLn <ok373737@naver.com>
2026-01-20 16:58:21 +0900

    feat: 분석 요청 생성 API 추가

---
+++ src/main/java/com/lh/assist/analysis/domain/AnalysisResultRepository.java
@@ -0,0 +1,6 @@
+package com.lh.assist.analysis.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalysisResultRepository extends JpaRepository<AnalysisResult, Long> {
+}
