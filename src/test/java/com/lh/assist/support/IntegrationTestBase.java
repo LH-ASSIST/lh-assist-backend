94483631c78d4583a871d4da9ffa5cda727e0957
SinnoLn <ok373737@naver.com>
2026-01-21 10:04:25 +0900

    test: 테스트 프로파일 및 통합 테스트 베이스 구성

--- src/test/java/com/lh/assist/support/IntegrationTestBase.java
+++ src/test/java/com/lh/assist/support/IntegrationTestBase.java
@@ -3,12 +3,14 @@ package com.lh.assist.support;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Testcontainers
 public abstract class IntegrationTestBase {
 
