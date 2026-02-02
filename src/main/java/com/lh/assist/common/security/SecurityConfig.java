6676e9e27fb05f4e777435f0266e01b762d6ea79
SinnoLn <ok373737@naver.com>
2026-02-02 16:43:28 +0900

    fix: 로컬 CORS 기본 허용 도메인 설정

--- src/main/java/com/lh/assist/common/security/SecurityConfig.java
+++ src/main/java/com/lh/assist/common/security/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(originList);
+		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
 		configuration.setAllowedHeaders(List.of("*"));
